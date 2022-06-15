0b2fa7296c077b9e45780948e1d6b2961a8b0fc5
Chang Chia Jian <95138758+CJinthecloud@users.noreply.github.com>
2022-06-15 21:27:55 +0800

    Add files via upload

---
+++ BitLocker/Pre_Check_BDE.ps1
@@ -0,0 +1,6 @@
+﻿$BitLockerVolume = Get-BitLockerVolume  -ErrorAction Stop | where {$_.ProtectionStatus -ne "off"} | where {$_.AutoUnlockEnabled -ne $null}
+if($BitLockerVolume){
+        return 1
+    }else{
+        return 2
+    }
