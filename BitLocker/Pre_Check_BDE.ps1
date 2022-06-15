3065bd311a29755a08d710c6981e1d02e559931c
Chang Chia Jian <95138758+CJinthecloud@users.noreply.github.com>
2022-06-15 21:35:40 +0800

    Update Pre_Check_BDE.ps1

--- BitLocker/Pre_Check_BDE.ps1
+++ BitLocker/Pre_Check_BDE.ps1
@@ -1,14 +1,14 @@
 ######################################################################################################################################
 ###
-### Name:           : UltimateEscrowBitLockerKey.ps1
+### Name:           : Pre_Check_BDE.ps1
 ### Created by      : Chang Chia Jian
-### Created on      : 06/06/2022
+### Created on      : 15/06/2022
 ### Latest Version  : 0.1
 ### Version History : 0.1 First version of the script                   
 ###
 ######################################################################################################################################
  
- 
+#Check BitLocker Drive Encryption
 $BitLockerVolume = Get-BitLockerVolume  -ErrorAction Stop | where {$_.ProtectionStatus -ne "off"} | where {$_.AutoUnlockEnabled -ne $null}
 if($BitLockerVolume){
         return 1
