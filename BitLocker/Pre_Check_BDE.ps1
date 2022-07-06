6d53f2e46f92719c99919ee780650629a334a02d
Chang Chia Jian <95138758+CJinthecloud@users.noreply.github.com>
2022-07-07 00:20:18 +0800

    Update Pre_Check_BDE.ps1

--- BitLocker/Pre_Check_BDE.ps1
+++ BitLocker/Pre_Check_BDE.ps1
@@ -1,13 +1,19 @@
-######################################################################################################################################
-###
-### Name:           : Pre_Check_BDE.ps1
-### Created by      : Chang Chia Jian
-### Created on      : 15/06/2022
-### Latest Version  : 0.1
-### Version History : 0.1 First version of the script                   
-###
-######################################################################################################################################
- 
+<#
+.SYNOPSIS
+    This script is to check the BitLocker drive status
+.DESCRIPTION
+    This script will detect the BitLocker Drive and define the decision to run escrow BitLocker
+.NOTES
+    File name: Pre_Check_BDE.ps1
+    VERSION: 1.1.0
+    AUTHOR: Chang Chia Jian
+    Created:  2022-06-15
+    Licensed under the MIT license.
+    Please credit me if you fint this script useful and do some cool things with it.
+.VERSION HISTORY:
+    1.0.0 - (2022-06-15) Script created
+#>
+
 #Check BitLocker Drive Encryption
 $BitLockerVolume = Get-BitLockerVolume  -ErrorAction Stop | where {($_.ProtectionStatus -eq "on") -and ($_.AutoUnlockEnabled -ne "false") -or ($_.AutoUnlockEnabled -eq "true")}
 if($BitLockerVolume){
